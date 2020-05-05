a916eca052b84ccbb0653651498e1a88a3de1389
mr-scrpt <mr.scrpt@gmail.com>
2020-05-05 20:34:48 +0300

    less 4 ready

--- scripts/webpack/constants.js
+++ scripts/webpack/constants.js
@@ -1,19 +1,9 @@
 import { resolve } from "path";
 import { path as PROJECT_ROOT } from "app-root-path";
 
-//expots const PROJECT_ROOT = PROJECT_ROOT;
-
 export { PROJECT_ROOT };
 export const SOURCE_DIRECTORY = resolve(PROJECT_ROOT, "./src");
 export const BUILD_DIRECTORY = resolve(PROJECT_ROOT, "./build");
 
 export const HOST = "localhost";
 export const PORT = 3000;
-
-/* export const PROJECT_ROOT = PROJECT_ROOT;
-export const SOURCE_DIRECTORY = resolve(PROJECT_ROOT, "./src");
-export const BUILD_DIRECTORY = resolve(PROJECT_ROOT, "./build");
-
-export const HOST = "localhost";
-export const PORT = 3000;
- */
